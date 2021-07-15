16ffd057f8395df09f7ecd0502cd0dbaf1bb1d12
Tom Hartung <tomwhartung@gmail.com>
2021-07-15 17:00:36 -0600

    Adding possibly_the_key.js , javascript source code found by viewing the source for one of the questions in the OEPS questionnaire.

---
+++ questionnaires/oeps/possibly_the_key.js
@@ -0,0 +1,98 @@
+
+var itemtext = 
+
+{
+
+ // 1
+
+ "Q1" : "I am a perfectionist.",
+
+ "Q2" : "I strive for efficiency.",
+
+ "Q3" : "I often have to redo other peoples work.",
+
+ "Q4" : "I keep my belongings in order.",
+
+ // 2
+
+ "Q5" : "My relationships with others are what my life is about.",
+
+ "Q6" : "I have difficulty saying no.",
+
+ "Q7" : "I get lots of satisfaction from helping others achieve their goals.",
+
+ "Q8" : "I put family first.",
+
+ // 3
+
+ "Q9" : "I put work first.",
+
+ "Q10" : "I like to stand out.",
+
+ "Q11" : "It is good to wake up to a full day of planned activities.",
+
+ "Q12" : "Money is important to my happiness.",
+
+ // 4
+
+ "Q13" : "I daydream about being in love.",
+
+ "Q14" : "I really enjoy feeling bitter sweet.",
+
+ "Q15" : "I cry.",
+
+ "Q16" : "I side with the rebels over the establishment.",
+
+ // 5
+
+ "Q17" : "I have a hard time showing emotions.",
+
+ "Q18" : "I spend hours alone with my hobbies.",
+
+ "Q19" : "I spend most of my time trying to understand things.",
+
+ "Q20" : "I like mental challenges.",
+
+ // 6
+
+ "Q21" : "Fear of being taken advantage of keeps me from being more trusting.",
+
+ "Q22" : "I get input from others before I make a decision.",
+
+ "Q23" : "I conform.",
+
+ "Q24" : "I am loyal.",
+
+ // 7
+
+ "Q25" : "I must always be having new experiences.",
+
+ "Q26" : "I can keep a conversation going with anyone about anything.",
+
+ "Q27" : "I am uninhibited.",
+
+ "Q28" : "I always try to break the tension with a good joke.",
+
+ // 8
+
+ "Q29" : "I naturally emerge as a leader.",
+
+ "Q30" : "I like a conversation where no one agrees.",
+
+ "Q31" : "I want people to tell me the truth, not spare my feelings.",
+
+ "Q32" : "I prefer it when leaders are decisive.",
+
+ // 9
+
+ "Q33" : "When other people are arguing, I leave the room.",
+
+ "Q34" : "I keep my thoughts to myself, to prevent trouble.",
+
+ "Q35" : "I am very accepting and flexible.",
+
+ "Q36" : "I avoid confrontation."
+
+};
+
+
